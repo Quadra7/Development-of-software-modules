bc9626bbdce8b4907aae8c9ebe3359ece4892cc5
Quadra <111442599+Quadra7@users.noreply.github.com>
2023-02-09 22:28:34 +0700

    Add files via upload

---
+++ lab_2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace lesson_02022023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonSecond = new System.Windows.Forms.Button();
+            this.buttonThird = new System.Windows.Forms.Button();
+            this.buttonFourth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelX.Location = new System.Drawing.Point(51, 9);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(148, 19);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "Введите значение X:";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelY.Location = new System.Drawing.Point(51, 48);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(147, 19);
+            this.labelY.TabIndex = 1;
+            this.labelY.Text = "Введите значение Y:";
+            // 
+            // labelZ
+            // 
+            this.labelZ.AutoSize = true;
+            this.labelZ.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelZ.Location = new System.Drawing.Point(51, 91);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(146, 19);
+            this.labelZ.TabIndex = 2;
+            this.labelZ.Text = "Введите значение Z:";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxX.Location = new System.Drawing.Point(205, 6);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 26);
+            this.textBoxX.TabIndex = 3;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxY.Location = new System.Drawing.Point(204, 45);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 26);
+            this.textBoxY.TabIndex = 4;
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxZ.Location = new System.Drawing.Point(203, 88);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(100, 26);
+            this.textBoxZ.TabIndex = 5;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(51, 136);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(245, 19);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Результат выполнения программы:";
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.Location = new System.Drawing.Point(55, 158);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.Size = new System.Drawing.Size(248, 163);
+            this.richTextBoxResult.TabIndex = 7;
+            this.richTextBoxResult.Text = "";
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Location = new System.Drawing.Point(51, 327);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(252, 23);
+            this.buttonFirst.TabIndex = 8;
+            this.buttonFirst.Text = "Уравнение №1";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            // 
+            // buttonSecond
+            // 
+            this.buttonSecond.Location = new System.Drawing.Point(51, 356);
+            this.buttonSecond.Name = "buttonSecond";
+            this.buttonSecond.Size = new System.Drawing.Size(252, 23);
+            this.buttonSecond.TabIndex = 9;
+            this.buttonSecond.Text = "Уравнение №2";
+            this.buttonSecond.UseVisualStyleBackColor = true;
+            this.buttonSecond.Click += new System.EventHandler(this.buttonSecond_Click);
+            // 
+            // buttonThird
+            // 
+            this.buttonThird.Location = new System.Drawing.Point(51, 385);
+            this.buttonThird.Name = "buttonThird";
+            this.buttonThird.Size = new System.Drawing.Size(252, 23);
+            this.buttonThird.TabIndex = 10;
+            this.buttonThird.Text = "Уравнение №3";
+            this.buttonThird.UseVisualStyleBackColor = true;
+            this.buttonThird.Click += new System.EventHandler(this.buttonThird_Click);
+            // 
+            // buttonFourth
+            // 
+            this.buttonFourth.Location = new System.Drawing.Point(51, 414);
+            this.buttonFourth.Name = "buttonFourth";
+            this.buttonFourth.Size = new System.Drawing.Size(252, 23);
+            this.buttonFourth.TabIndex = 11;
+            this.buttonFourth.Text = "Уравнение №4";
+            this.buttonFourth.UseVisualStyleBackColor = true;
+            this.buttonFourth.Click += new System.EventHandler(this.buttonFourth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.buttonFourth);
+            this.Controls.Add(this.buttonThird);
+            this.Controls.Add(this.buttonSecond);
+            this.Controls.Add(this.buttonFirst);
+            this.Controls.Add(this.richTextBoxResult);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.labelZ);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Name = "Form1";
+            this.Text = "Лаб. раб. №2 гр. 107з2 Монич Александр";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelZ;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonSecond;
+        private System.Windows.Forms.Button buttonThird;
+        private System.Windows.Forms.Button buttonFourth;
+    }
+}
+
---
+++ lab_2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_02022023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        // Buttons
+        //
+        private void buttonFirst_Click(object sender, EventArgs e) // Приблизительно, но не сошлось
+        {
+            try
+            {
+                double X = Convert.ToDouble(textBoxX.Text);
+                double Y = Convert.ToDouble(textBoxY.Text);
+                double Z = Convert.ToDouble(textBoxZ.Text);
+                double t = ((2 * Math.Cos(X - Math.PI / 6)) / (0.5 + Math.Sin(Y) * Math.Sin(Y))) * (1 + (Z * Z) / (3 - Z * Z / 5));
+                richTextBoxResult.Text = $"X = {X}\nY = {Y}\nZ = {Z}\nРезультат T = {t}";
+            }
+            catch
+            {
+                MessageBox.Show("Wrong numbers", "Wrong", MessageBoxButtons.OK);
+            }
+        }
+        private void buttonSecond_Click(object sender, EventArgs e) // Приблизительно, но не сошлось
+        {
+            try
+            {
+                double X = Convert.ToDouble(textBoxX.Text);
+                double Y = Convert.ToDouble(textBoxY.Text);
+                double Z = Convert.ToDouble(textBoxZ.Text);
+                double u = (Math.Pow(8 + Math.Pow(Math.Abs(X - Y), 2) + 1, (1 / 3))) / (X * X + Y * Y + 2) - Math.Exp(Math.Abs(X - Y)) * Math.Pow(Math.Tan(Z) * Math.Tan(Z) + 1, X);
+                richTextBoxResult.Text = $"X = {X}\nY = {Y}\nZ = {Z}\nРезультат U = {u}";
+            }
+            catch
+            {
+                MessageBox.Show("Wrong numbers", "Wrong", MessageBoxButtons.OK);
+            }
+        }
+        private void buttonThird_Click(object sender, EventArgs e) // Сошлось
+        {
+            try
+            {
+                double X = Convert.ToDouble(textBoxX.Text);
+                double Y = Convert.ToDouble(textBoxY.Text);
+                double Z = Convert.ToDouble(textBoxZ.Text);
+                double v = (1 + Math.Sin(X + Y) * Math.Sin(X + Y)) / (Math.Abs(X - (2 * Y) / (1 + X * X * Y * Y))) * Math.Pow(X, Math.Abs(Y)) + Math.Cos(Math.Atan(1 / Z)) * Math.Cos(Math.Atan(1 / Z));
+                richTextBoxResult.Text = $"X = {X}\nY = {Y}\nZ = {Z}\nРезультат V = {v}";
+            }
+            catch
+            {
+                MessageBox.Show("Wrong numbers", "Wrong", MessageBoxButtons.OK);
+            }
+        }
+        private void buttonFourth_Click(object sender, EventArgs e) // Сошлось
+        {
+            try
+            {
+                double X = Convert.ToDouble(textBoxX.Text);
+                double Y = Convert.ToDouble(textBoxY.Text);
+                double Z = Convert.ToDouble(textBoxZ.Text);
+                double w = Math.Pow(Math.Abs(Math.Cos(X) - Math.Cos(Y)), 1 + 2 * Math.Sin(Y) * Math.Sin(Y)) * (1 + Z + Z * Z / 2 + Z * Z * Z / 3 + Z * Z * Z * Z / 4);
+                richTextBoxResult.Text = $"X = {X}\nY = {Y}\nZ = {Z}\nРезультат W = {w}";
+            }
+            catch
+            {
+                MessageBox.Show("Wrong numbers", "Wrong", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
