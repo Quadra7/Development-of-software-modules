e5bb5c4256d0a28491379fa1500abf9a6eaeb65c
Quadra <111442599+Quadra7@users.noreply.github.com>
2023-03-16 12:59:19 +0700

    Add files via upload

---
+++ lab_9 changed/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_02022023
+{
+    public partial class Form1 : Form
+    {
+        int k = 1;
+        double f = 0.5;
+        double X0 = 0;
+        double Xk = 7;
+        double dx = 0.1;
+        int count;
+        double[] x;
+        double[] y1;
+        double[] y2;
+        public Form1()
+        {
+            InitializeComponent();
+            count = (int)Math.Ceiling((Xk - X0) / dx) + 1;
+            x = new double[count];
+            y1 = new double[count];
+            y2 = new double[count];
+            label1.Text = "";
+            chart1.ChartAreas[0].AxisX.Minimum = -1;
+            chart1.ChartAreas[0].AxisX.Maximum = 1;
+            chart1.ChartAreas[0].AxisY.Minimum = -1;
+            chart1.ChartAreas[0].AxisY.Maximum = 1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = dx;
+        }
+        //
+        // Buttons
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int k = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Wrong number", "Wrong", MessageBoxButtons.OK);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                k = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                k = 1;
+            }
+            f += 0.1;
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = X0 + dx * i;
+                y1[i] = Math.Sin(x[i]);
+                y2[i] = Math.Cos(k * x[i] + f);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = -1;
+            chart1.ChartAreas[0].AxisX.Maximum = 1;
+            chart1.ChartAreas[0].AxisY.Minimum = -1;
+            chart1.ChartAreas[0].AxisY.Maximum = 1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = dx;
+            chart1.Series[0].Points.DataBindXY(y1, y2);
+            label1.Text = $"{f}\n";
+            Invalidate();
+        }
+    }
+}
---
+++ lab_9 changed/README.txt
@@ -0,0 +1,4 @@
+Лабораторная работа №9
+
+y1 = 1.2 * Math.Pow(a - b, 3) * Math.Pow(Math.Exp(x[i]), 2) + x[i];
+y2 = Math.Abs(x[i] / 2) - ((3 * Math.Sqrt(33) - 7) / 112) * x[i] * x[i] - 3; 
