2ef0aad109c6acf114b9b1bcd1a75f5028b649e1
Quadra <111442599+Quadra7@users.noreply.github.com>
2023-03-18 00:01:12 +0700

    Add files via upload

--- lab_9 changed/Form1.Designer.cs
+++ lab_9 changed/Form1.Designer.cs
@@ -29,46 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.buttonCalc = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea8.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea8);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
-            series15.ChartArea = "ChartArea1";
-            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            series15.Name = "Function #1";
-            series16.ChartArea = "ChartArea1";
-            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series16.Name = "Function #2";
-            this.chart1.Series.Add(series15);
-            this.chart1.Series.Add(series16);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series3.Name = "Function #1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Function #2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
             this.chart1.Size = new System.Drawing.Size(508, 412);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            // 
-            // buttonCalc
-            // 
-            this.buttonCalc.Location = new System.Drawing.Point(651, 12);
-            this.buttonCalc.Name = "buttonCalc";
-            this.buttonCalc.Size = new System.Drawing.Size(121, 43);
-            this.buttonCalc.TabIndex = 1;
-            this.buttonCalc.Text = "Построить график";
-            this.buttonCalc.UseVisualStyleBackColor = true;
-            this.buttonCalc.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -83,24 +71,13 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(625, 191);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.buttonCalc);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Лаб. раб. №9 гр. 107з2 Монич Александр";
@@ -113,10 +90,8 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button buttonCalc;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- lab_9 changed/Form1.cs
+++ lab_9 changed/Form1.cs
@@ -12,12 +12,8 @@ namespace lesson_02022023
 {
     public partial class Form1 : Form
     {
-        int k = 1;
-        double f = 0.5;
-        double X0 = 0;
-        double Xk = 7;
-        double dx = 0.1;
-        int count;
+        int count, k = 1;
+        double f = 0.5, X0 = 0, Xk = 7, dx = 0.1;
         double[] x;
         double[] y1;
         double[] y2;
@@ -28,7 +24,6 @@ namespace lesson_02022023
             x = new double[count];
             y1 = new double[count];
             y2 = new double[count];
-            label1.Text = "";
             chart1.ChartAreas[0].AxisX.Minimum = -1;
             chart1.ChartAreas[0].AxisX.Maximum = 1;
             chart1.ChartAreas[0].AxisY.Minimum = -1;
@@ -36,22 +31,11 @@ namespace lesson_02022023
             chart1.ChartAreas[0].AxisX.MajorGrid.Interval = dx;
         }
         //
-        // Buttons
+        // Timers
         //
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int k = Convert.ToInt32(textBox1.Text);
-            }
-            catch
-            {
-                MessageBox.Show("Wrong number", "Wrong", MessageBoxButtons.OK);
-            }
-        }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
+            f += 0.1;
             try
             {
                 k = Convert.ToInt32(textBox1.Text);
@@ -60,20 +44,13 @@ namespace lesson_02022023
             {
                 k = 1;
             }
-            f += 0.1;
             for (int i = 0; i < count; i++)
             {
                 x[i] = X0 + dx * i;
                 y1[i] = Math.Sin(x[i]);
                 y2[i] = Math.Cos(k * x[i] + f);
             }
-            chart1.ChartAreas[0].AxisX.Minimum = -1;
-            chart1.ChartAreas[0].AxisX.Maximum = 1;
-            chart1.ChartAreas[0].AxisY.Minimum = -1;
-            chart1.ChartAreas[0].AxisY.Maximum = 1;
-            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = dx;
             chart1.Series[0].Points.DataBindXY(y1, y2);
-            label1.Text = $"{f}\n";
             Invalidate();
         }
     }
